4e90a02d7bc8b8ea7e56084505ba3db294930999
andres-ramirez <andres.boico@gmail.com>
2025-05-19 11:48:11 -0500

    Preparar despliegue en GitHub Pages

--- bot-restaurante-demo/src/lib/supabaseClient.js
+++ bot-restaurante-demo/src/lib/supabaseClient.js
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+
 // // ⚠️ Reemplaza estos valores con los de tu proyecto en Supabase
-const supabaseUrl = 'https://czmuzwyyqrmugdoynrbu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6bXV6d3l5cXJtdWdkb3lucmJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYzODQyMjMsImV4cCI6MjA1MTk2MDIyM30.rDAPgRbRcLWfFBnJq-xMp9XRZ6v3iTLhKEgMxmc7HEY';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
