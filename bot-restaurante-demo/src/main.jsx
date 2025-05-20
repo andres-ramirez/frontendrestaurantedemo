cc833ce47db242d64105dc91873aad0a42f2194d
andres-ramirez <andres.boico@gmail.com>
2025-05-20 09:28:11 -0500

    sin basename

--- bot-restaurante-demo/src/main.jsx
+++ bot-restaurante-demo/src/main.jsx
@@ -15,7 +15,7 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/frontendrestaurantedemo/">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
